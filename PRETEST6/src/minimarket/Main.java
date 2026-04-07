72a1d5f16638b1663aafcf6d0904df16056e7c07
mrizkiruswan-lab <mrizkiruswan@gmail.com>
2026-04-07 15:05:43 +0700

    Modul6

---
+++ PRETEST6/src/minimarket/Main.java
@@ -0,0 +1,14 @@
+package minimarket;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MakananRingan snack = new MakananRingan(
+                "Keripik Kentang",
+                12000,
+                "Balado"
+        );
+
+        snack.tampilkanDetail();
+    }
+}
